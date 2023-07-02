77b52d42a15bfde8c65a29effc8467d013918ab2
minakshimathpal <bhatt.minakshi@gmail.com>
2023-07-02 19:10:14 +0000

    Added image_folder_module, image_folder.yaml,experiments folder

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     install_requires=["lightning", "hydra-core"],# this will look for requirements.txt
     packages=find_packages(), # find all the packages in your module 
     # use this to customize global commands available in the terminal after installing the package
-    # entry_points={
-    #     "console_scripts": [
-    #         "copper_train = copper.train:main",
-    #         "copper_eval = copper.eval:main",
-    #     ]
-    # },
+    entry_points={
+        "console_scripts": [
+            "copper_train = copper.train:main",
+            "copper_eval = copper.eval:main",
+        ]
+    },
 )
