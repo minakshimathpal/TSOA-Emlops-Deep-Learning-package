b53da08620741ae83b6b2bece2cdef47542b2b96
minakshimathpal <bhatt.minakshi@gmail.com>
2023-07-02 20:39:00 +0000

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="emloCarVsDog",
+    name="classifier",
     version="0.0.1",
     description="PyTorch Lightning Cat vs Dog Classifier",
     author="Minakshi",
